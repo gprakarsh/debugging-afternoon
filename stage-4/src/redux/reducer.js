8af465963bcfc4162ec3cbf75e9e4427368a09b4
Your Name <you@example.com>
2019-02-05 15:05:57 -0700

    All Done

--- stage-4/src/redux/reducer.js
+++ stage-4/src/redux/reducer.js
@@ -1,4 +1,4 @@
-import * as productsController from './productsController';
+import getAll from './productsController';
 
 // Constants
 const ADD_TO_SHOPPING_CART = "ADD_TO_SHOPPING_CART";
@@ -13,7 +13,7 @@ let initialState = {
 }
 
 // Reducer
-export default function reducer(state, action) {
+export default function reducer(state=initialState, action) {
     switch(action.type) {
         case GET_ALL_PRODUCTS_PENDING:
             return Object.assign({}, state, {loading: true});
@@ -52,6 +52,6 @@ export function removeFromShoppingCart(productIndex) {
 export function getAllProducts(products) {
     return {
         type: GET_ALL_PRODUCTS,
-        payload: productsController.getAllProducts()
+        payload: getAll()
     }
 }
